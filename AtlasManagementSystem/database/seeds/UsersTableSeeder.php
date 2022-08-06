692695f40777168253c416fab185b295b6466df1
のすけ <>
2022-08-06 12:48:15 +0900

    テストユーザー作成用seeder編集

--- AtlasManagementSystem/database/seeds/UsersTableSeeder.php
+++ AtlasManagementSystem/database/seeds/UsersTableSeeder.php
@@ -14,11 +14,11 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'over_name' => '田中',
-            'under_name' => '太郎',
-            'over_name_kana' => 'タナカ',
-            'under_name_kana' => 'タロウ',
-            'mail_address' => 'Tanaka@atlas.com',
+            'over_name' => 'ユーザー',
+            'under_name' => '一',
+            'over_name_kana' => 'ユーザー',
+            'under_name_kana' => 'イチ',
+            'mail_address' => '1@atlas.com',
             'sex' => '1',
             'birth_day' => '2000-01-01',
             'role' => '4',
